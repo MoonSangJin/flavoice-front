04c5ee3b5df76d9c8ea2be5ad16a46c16a8afe27
MoonSangJin <1128msj@naver.com>
2021-05-30 15:00:03 +0900

    feat/presenter추가

---
+++ src/Routes/Home/HomePresenter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePresenter = () => {
+  return <div>HomePresenter</div>;
+};
+
+export default HomePresenter;
---
+++ src/Routes/SignUp/SignUpPresenter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUpPresenter = () => {
+  return <div>SignUpPresenter</div>;
+};
+
+export default SignUpPresenter;
