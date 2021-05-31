e7d7ea494c943085cb96798be46c58d4ccd54807
MoonSangJin <1128msj@naver.com>
2021-05-31 16:35:27 +0900

    feat/recorder presenter 생성

--- src/Routes/Recorder/RecorderPresenter.js
+++ src/Routes/Recorder/RecorderPresenter.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { useReactMediaRecorder } from 'react-media-recorder';
+
 const RecorderPresenter = () => {
-  return <div>RecorderPresenter</div>;
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true });
+
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
 };
 
 export default RecorderPresenter;
