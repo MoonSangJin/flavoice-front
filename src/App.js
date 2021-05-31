f899c52b94bc2b6a130877e778441db1fefc0b15
MoonSangJin <1128msj@naver.com>
2021-05-31 16:33:15 +0900

    feat/router 생성 및 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import { useReactMediaRecorder } from 'react-media-recorder';
-import Home from './Routes/Home';
-const RecordView = () => {
-  const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ audio: true });
+import React from 'react';
+import Router from './Components/Router';
 
-  const showType = async () => {
-    console.log(mediaBlobUrl);
-    let blob = await fetch(mediaBlobUrl).then((r) => r.blob());
-    console.log(blob);
-  };
-
+const App = () => {
   return (
-    <div>
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <button onClick={showType}>audio 확인하기</button>
-      <audio src={mediaBlobUrl} controls autoPlay loop />
-      <Home />
-    </div>
+    <>
+      <Router />
+    </>
   );
 };
 
-export default RecordView;
+export default App;
