04501b4fb1739be19ba0ce5ff95b4497fd4979c4
MoonSangJin <1128msj@naver.com>
2021-06-05 19:28:27 +0900

    feat/backbutton component제작

---
+++ src/components/BackButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BackButton = () => {
+  return (
+    <>
+      <Container>Back</Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  width: 100px;
+  background-color: blue;
+`;
+export default BackButton;
