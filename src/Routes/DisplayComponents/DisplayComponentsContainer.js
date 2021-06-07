0db46fde27b720c541e5d62fb0fb7d9de011a2f8
MoonSangJin <1128msj@naver.com>
2021-06-07 19:31:24 +0900

    feat/add DisplayComponents Interface

---
+++ src/Routes/DisplayComponents/DisplayComponentsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import DisplayComponentsPresenter from './DisplayComponentsPresenter';
+const DisplayComponentsContainer = () => {
+  return <DisplayComponentsPresenter />;
+};
+
+export default DisplayComponentsContainer;
---
+++ src/Routes/DisplayComponents/DisplayComponentsPresenter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DisplayComponentsPresenter = () => {
+  return <div>DisplayComponentsPresenter</div>;
+};
+
+export default DisplayComponentsPresenter;
