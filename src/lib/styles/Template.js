922f08913d8acbede1adf98f37014817be263490
simyeen <simyeen@naver.com>
2021-06-10 18:37:33 +0900

    fix/fontSize(media)

--- src/lib/styles/Template.js
+++ src/lib/styles/Template.js
@@ -2,44 +2,42 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+
 @media (max-width: 262px) {
   html {
-    font-size: 14px;
+    font-size: 7px;
   }
 }
 @media (min-width: 263px) and (max-width: 300px) {
   html {
-    font-size: 15px;
+    font-size: 8px;
   }
 }
 @media (min-width: 301px) and (max-width: 337px) {
   html {
-    font-size: 17px;
+    font-size: 9px;
   }
 }
 @media (min-width: 338px) and (max-width: 375px) {
   html {
-    font-size: 18px;
+    font-size: 10px;
   }
 }
-@media (min-width: 376px) and (max-width: 450px) {
+@media (min-width: 376px) and (max-width: 412px) {
   html {
-    font-size: 18px;
+    font-size: 11px;
   }
 }
- 
+@media (min-width: 413px) and (max-width: 450px) {
+  html {
+    font-size: 12px;
+  }
+}
 @media (min-width: 451px){
   html {
-    font-size: 18px;
+    font-size: 13px;
   }
-} 
-/* 
-@media (max-width: 1024px){
-  width : 768px;
 }
-@media (max-width: 768px){
-  width: 100%;
-} */
 
 
 ::-webkit-scrollbar {
@@ -66,7 +64,7 @@ const TemplateBlock = styled.div`
 
 const WhiteBox = styled.div`
   width: 100vw;
-  max-width: 36rem;
+  max-width: 37.5rem;
   min-height: 100vh;
   background-color: white;
   position: relatvie;
