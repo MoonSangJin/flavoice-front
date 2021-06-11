abc0ff2c0d22f2e411b377404191510317c1019a
MoonSangJin <1128msj@naver.com>
2021-06-11 21:22:35 +0900

    feat/BackButton 수정

--- src/Components/BackButton.js
+++ src/Components/BackButton.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 import palette from '../lib/styles/paletts';
 import Text from './Text';
 
-
-const BackButton = () => {
+const BackButton = ({ width = 50, height = 30 }) => {
   return (
     <>
-      <Container>
+      <Container {...{ width, height }}>
         <Text hover>Back</Text>
       </Container>
     </>
@@ -15,9 +14,44 @@ const BackButton = () => {
 };
 
 const Container = styled.div`
-  width: 100px;
+  background: ${palette.orange};
+  color: #fff;
+  border: none;
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  position: relative;
+  font-size: 1.6em;
+  padding: 0 2em;
+  cursor: pointer;
+  transition: 800ms ease all;
+  outline: none;
+  margin: 5px;
 
-  background-color: ${palette.orange};
-
+  &:hover {
+    background: #fff;
+    color: ${palette.orange};
+    ::before,
+    ::after {
+      width: 100%;
+      transition: 800ms ease all;
+    }
+  }
+  ::before,
+  ::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 2px;
+    width: 0;
+    background: ${palette.orange};
+    transition: 400ms ease all;
+  }
+  ::after {
+    right: inherit;
+    top: inherit;
+    left: 0;
+    bottom: 0;
+  }
 `;
 export default BackButton;
