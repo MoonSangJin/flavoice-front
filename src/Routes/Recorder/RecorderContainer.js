d377ac25637b512d346532834174267a71438832
simyeen <simyeen@naver.com>
2021-06-11 20:14:38 +0900

    feat/upload

--- src/Routes/Recorder/RecorderContainer.js
+++ src/Routes/Recorder/RecorderContainer.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import RecorderPresenter from './RecorderPresenter';
+import { useReactMediaRecorder } from 'react-media-recorder';
 
 const RecorderContainer = () => {
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ audio: true });
+
+  const showType = async () => {
+    console.log(mediaBlobUrl);
+    let blob = await fetch(mediaBlobUrl).then((r) => r.blob());
+    console.log(blob);
+  };
+
+  const [imgUrls, setImgUrls] = useState([{ id: '', fileUrl: '' }]);
+  const MAX = 3;
+
+  const nextId = useRef(1);
+  const onInsert = (e) => {
+    if (imgUrls.length > MAX) {
+      alert('사진은 3개 까지만 업로드 가능합니다.');
+      return;
+    }
+
+    const fileArr = e.target.files;
+
+    let fileUrls = [];
+
+    let file;
+    let filesLength = fileArr.length;
+
+    for (let i = 0; i < filesLength; i++) {
+      file = fileArr[i];
+
+      let reader = new FileReader();
+      reader.onload = () => {
+        fileUrls[i] = reader.result;
+        const image = {
+          id: nextId.current,
+          fileUrl: fileUrls[i],
+        };
+
+        console.log('for in image i nextId.current ', image, i, nextId.current);
+
+        setImgUrls(imgUrls.concat(image));
+        nextId.current += 1;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const onRemove = (id) => {
+    console.log('onRemove', imgUrls, id);
+    setImgUrls(imgUrls.filter((img) => img.id !== id));
+  };
+
   return (
     <div>
-      <RecorderPresenter />
+      <RecorderPresenter
+        {...{ status }}
+        {...{ showType }}
+        {...{ imgUrls }}
+        {...{ mediaBlobUrl }}
+        {...{ startRecording }}
+        {...{ stopRecording }}
+        {...{ onInsert }}
+        {...{ onRemove }}
+      />
     </div>
   );
 };
--- src/Routes/Recorder/RecorderPresenter.js
+++ src/Routes/Recorder/RecorderPresenter.js
@@ -1,27 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useReactMediaRecorder } from 'react-media-recorder';
 import BackButton from '../../Components/BackButton';
 import Button from '../../Components/Button';
 import Text from '../../Components/Text';
 import Form from '../../Components/Form';
+import styled from 'styled-components';
+import palette from '../../lib/styles/paletts';
 
-const RecorderPresenter = () => {
-  const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ audio: true });
+const ImBlock = styled.div`
+  position: relative;
+`;
 
-  const showType = async () => {
-    console.log(mediaBlobUrl);
-    let blob = await fetch(mediaBlobUrl).then((r) => r.blob());
-    console.log(blob);
-  };
+const StyledImg = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 8px;
+`;
+
+const CloseBox = styled(Button)`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: ${palette.black};
+`;
+
+const CameraBlock = styled.label`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 250px;
+  height: 250px;
+  padding: 15px 11px 7px 12px;
+  border-radius: 8px;
+  border: solid 1px ${palette.gray[100]};
+  background-color: ${palette.gray[50]};
+  letter-spacing: -0.7px;
+  color: #808080;
+
+  cursor: pointer;
+`;
+
+const RecorderPresenter = ({
+  onShowType,
+  status,
+  startRecording,
+  stopRecording,
+  mediaBlobUrl,
+  onInsert,
+  onRemove,
+  imgUrls,
+}) => {
   return (
     <Form>
       <Link to="/">
         <BackButton />
       </Link>
       <Text>{status}</Text>
-      <Button onClick={showType} hover>
+      <Button onClick={onShowType} hover>
         <Text hover>type확인버튼</Text>
       </Button>
       <Button onClick={startRecording}>
@@ -31,6 +70,27 @@ const RecorderPresenter = () => {
         <Text hover>녹화종료</Text>
       </Button>
       <audio src={mediaBlobUrl} controls />
+
+      <CameraBlock>
+        <Text fontWeight={400}>녹음파일추가</Text>
+        <input
+          style={{ display: 'none' }}
+          type="file"
+          multiple
+          accept="audio/*"
+          onChange={onInsert}
+        />
+      </CameraBlock>
+      {imgUrls.map((imgUrl, i) => (
+        <div key={i + 1}>
+          {i === 0 ? null : (
+            <ImBlock>
+              <StyledImg src={imgUrl.fileUrl} />
+              <CloseBox onClick={() => onRemove(imgUrl.id)}></CloseBox>
+            </ImBlock>
+          )}
+        </div>
+      ))}
     </Form>
   );
 };
