f6ae0c93402af4698f3da6ad036aa337c4a8fa64
simyeen <simyeen@naver.com>
2021-06-11 15:04:07 +0900

    add paletts.js(color reop), Form.js

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import BackButton from '../../components/BackButton';
+import Form from '../../components/Form';
 
 const HomePresenter = () => {
   return (
     <>
       HomePresenter
       <Wrap>
-        <Link to="/">
-          <BackButton />
-        </Link>
-        <Link to="/">Home</Link>
-        <Link to="/signin">SignIn</Link>
-        <Link to="/signup">SignUp</Link>
-        <Link to="/recorder">Recorder</Link>
-        <Link to="/displayComponents">DisplayComponents</Link>
+        <Form>
+          <Link to="/">
+            <BackButton />
+          </Link>
+          <Link to="/">Home</Link>
+          <Link to="/signin">SignIn</Link>
+          <Link to="/signup">SignUp</Link>
+          <Link to="/recorder">Recorder</Link>
+          <Link to="/displayComponents">DisplayComponents</Link>
+        </Form>
       </Wrap>
     </>
   );
