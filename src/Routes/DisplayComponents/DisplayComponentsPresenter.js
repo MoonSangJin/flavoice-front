7d5df78fc1916d6899d1a7c2d193ca8b9685f864
MoonSangJin <1128msj@naver.com>
2021-06-11 16:21:37 +0900

    feat/checkbox 컴포넌트 최적화

--- src/Routes/DisplayComponents/DisplayComponentsPresenter.js
+++ src/Routes/DisplayComponents/DisplayComponentsPresenter.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import Form from '../../Components/Form';
+import BackButton from '../../Components/BackButton';
+import CheckBox from '../../Components/CheckBox';
 
 const DisplayComponentsPresenter = () => {
-  return <div>DisplayComponentsPresenter</div>;
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <>
+      <Form>
+        <Link to="/">
+          <BackButton />
+        </Link>
+        <CheckBox value={isChecked} onClick={() => setIsChecked(!isChecked)} />
+      </Form>
+    </>
+  );
 };
 
 export default DisplayComponentsPresenter;
