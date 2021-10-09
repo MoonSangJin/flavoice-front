59eecbff804c9967329d2f3a4e9fa397e5bc1c69
MoonSangJin <1128msj@naver.com>
2021-10-09 17:16:15 +0900

    refactor/access token 만료시 로직 추가

--- src/Routes/SignIn/SignInContainer.js
+++ src/Routes/SignIn/SignInContainer.js
@@ -10,8 +10,25 @@ const SignInContainer = () => {
     password: '',
   });
 
+  const JWT_ACCESS_EXPIRY_TIME = 5 * 60 * 1000; //5분 밀리초
+  const JWT_REFRESH_EXPIRY_TIME = 60 * 60 * 1000; //60분 밀리초
+
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const onSilentRefresh = async (refreshToken) => {
+    try {
+      const refreshResult = await axios.post(
+        'https://flavoice.shop/accounts/token/refresh/',
+        { refresh: refreshToken }
+      );
+      console.log('refresh 성공');
+      console.log(JSON.stringify(refreshResult));
+    } catch (e) {
+      console.log('refresh실패');
+      console.log(e);
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -23,16 +40,20 @@ const SignInContainer = () => {
         'https://flavoice.shop/accounts/login/',
         values
       );
-      console.log(result);
+      console.log(JSON.stringify(result));
 
       const { data } = result;
       const accessToken = data['access_token'];
-      const refreshToken = data['refresh_token']; //ToDo refreshToken 활용방안 : 유효시간 따라 다르게
       axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      const refreshToken = data['refresh_token']; //ToDo refreshToken 활용방안 : 유효시간 따라 다르게
       alert('로그인 완료');
       const userData = await axios.get('https://flavoice.shop/accounts/user');
       console.log(`user 정보 ${JSON.stringify(userData)}`);
       localStorage.setItem('token', accessToken);
+      setTimeout(
+        () => onSilentRefresh(refreshToken),
+        JWT_ACCESS_EXPIRY_TIME - 60000
+      ); //()=>{}랑 onSilentRefesh() 차이 확인필요
       history.push('/');
     } catch (e) {
       console.log(e);
