00c224ad9bcc393a497791f6b98ae180c7c4b824
MoonSangJin <1128msj@naver.com>
2021-10-31 10:26:58 +0900

    guide 화면 style 수정 및 홈화면에서 pitch 확인

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -4,16 +4,22 @@ import checkLogin from '../../util/checkLogin';
 import { useHistory } from 'react-router';
 const HomeContainer = () => {
   const [loginChecker, setLoginChecker] = useState(false);
+  const [userPitch, setUserPitch] = useState('0');
+
+  const checkUserPitch = () => {
+    setUserPitch(localStorage.getItem('pitch'));
+  };
 
   useEffect(() => {
     if (checkLogin()) {
       setLoginChecker(true);
     }
+    checkUserPitch();
   }, []);
 
   return (
     <div>
-      <HomePresenter {...{ loginChecker }} />
+      <HomePresenter {...{ loginChecker, userPitch }} />
     </div>
   );
 };
